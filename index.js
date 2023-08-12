ca03a08da12b80c1c21a15b00613f3997f5fea7c
izetter <59151193+izetter@users.noreply.github.com>
2023-08-11 20:43:47 -0600

    Working. pending max day validation

---
+++ index.js
@@ -0,0 +1,74 @@
+const year = document.querySelector('#year');
+const month = document.querySelector('#month');
+const day = document.querySelector('#day');
+const btn = document.querySelector('button');
+const output = document.querySelector('output');
+
+// const inputs = [year, month, day];
+
+function enableDisableDayInput() {
+	if (year.value && month.value) {
+		day.disabled = false;
+	} else {
+		day.value = '';
+		day.disabled = true;
+	}
+}
+
+function enableDisableBtn() {
+	if (day.value) {
+		btn.disabled = false;
+	} else {
+		btn.disabled = true;
+	}
+}
+
+// function isInputEmpty() {
+// 	if (!year.value || !month.value || !day.value) {
+// 		output.value = '';
+// 		inputs.forEach((input) => {
+// 			if (!input.value) {
+// 				input.classList.add('invalid');
+// 			} else {
+// 				input.classList.remove('invalid');
+// 			}
+// 		});
+// 	}
+// }
+
+function handleClick() {
+	const dayNumber = new Date(year.value, month.value - 1, day.value).getDay();
+
+	if (!year.value || !month.value || !day.value) {
+		output.value = '';	
+	} else {
+		switch (dayNumber) {
+			case 0:
+				output.value = 'Domingo, día no laborable';
+				break;
+			case 1:
+				output.value = 'Lunes, día laborable';
+				break;
+			case 2:
+				output.value = 'Martes, día laborable';
+				break;
+			case 3:
+				output.value = 'Miércoles, día laborable';
+				break;
+			case 4:
+				output.value = 'Jueves, día laborable';
+				break;
+			case 5:
+				output.value = 'Viernes, día laborable';
+				break;
+			case 6:
+				output.value = 'Sábado, día no laborable';
+				break;
+		}
+	}
+}
+
+year.addEventListener('input', enableDisableDayInput);
+month.addEventListener('input', enableDisableDayInput);
+day.addEventListener('input', enableDisableBtn);
+btn.addEventListener('click', handleClick);
